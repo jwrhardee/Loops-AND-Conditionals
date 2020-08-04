0983dc4ed60892ad5acab23bf8bf88f988939a4a
jesseHardee <jwrhardee@gmail.com>
2020-08-04 19:41:23 +0000

    added loops and conditionals

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 print("loops and conditonals")
 
+# for loop while loop 
+#X IS A Loop varible 
+#for x in range(5):
+#   print(x)
+
+for x in range(3, 6):
+  print (x)
+
+
+#while loop
+count = 0
+while (count < 3):
+  print("hello")
+  print (count)
+  count += 1
+
+# Conditionals
+
+i = 20
+if (i > 15):
+  print ("i is more than 15")
+  print ("i'm in a if block")
+elif (i == 15):
+  print ("i is equal to 15")
+  print ("i'm a elif")
+else: 
+  print ("i is less then 15")
+  print ("i'm in a else block")
